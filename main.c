d9b32a23558ec5e7cc36e9f0cc5cf6b11d8a467f
tylenter <manhht10ht@gmail.com>
2025-09-28 14:58:41 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,58 +1,128 @@
-//dieu khien 8 led
-#include "stm32f10x_gpio.h"
-#include "stm32f10x_rcc.h"
+#include "stm32f10x.h"
+#include <stdio.h>
 
-uint16_t sangdan[8]={0xFE, 0xFC, 0xF8, 0xF0, 0xE0, 0xC0, 0x80, 0x00};
-void Delay(uint32_t);
+#define ADC_BUFFER_SIZE 16
+uint16_t adc_buffer[ADC_BUFFER_SIZE];
+
+void RCC_Config(void);
 void GPIO_Config(void);
-void Clock_Config(void);
+void USART1_Config(void);
+void ADC_Config(void);
+void DMA_Config(void);
+void delay_ms(uint32_t ms);
+void send_data(uint16_t *data, uint16_t size);
 
-int main(void){
-    Clock_Config(); // configuraion clock
-    SystemCoreClockUpdate(); // update SystemCoreClock varibale
+int main(void) {
+    RCC_Config();
     GPIO_Config();
-    
-    while(1){
-        for( int i = 0; i < 8; i++){
-            GPIO_Write(GPIOC, sangdan[i]);
-            Delay(100);   
+    USART1_Config();
+    ADC_Config();
+    DMA_Config();
+
+    ADC_SoftwareStartConvCmd(ADC1, ENABLE); // 
+
+    while (1) {
+        delay_ms(1000); 
+        send_data(adc_buffer, ADC_BUFFER_SIZE); 
+    }
+}
+
+
+
+void RCC_Config(void) {
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_ADC1 | RCC_APB2Periph_USART1, ENABLE);
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+}
+
+void GPIO_Config(void) {
+    GPIO_InitTypeDef gpio;
+
+    gpio.GPIO_Pin = GPIO_Pin_0;
+    gpio.GPIO_Mode = GPIO_Mode_AIN;
+    GPIO_Init(GPIOA, &gpio);
+
+    gpio.GPIO_Pin = GPIO_Pin_9;
+    gpio.GPIO_Mode = GPIO_Mode_AF_PP;
+    gpio.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &gpio);
+}
+
+void USART1_Config(void) {
+    USART_InitTypeDef usart;
+    usart.USART_BaudRate = 9600;
+    usart.USART_WordLength = USART_WordLength_8b;
+    usart.USART_StopBits = USART_StopBits_1;
+    usart.USART_Parity = USART_Parity_No;
+    usart.USART_Mode = USART_Mode_Tx;
+    usart.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_Init(USART1, &usart);
+    USART_Cmd(USART1, ENABLE);
+}
+
+void ADC_Config(void) {
+    ADC_InitTypeDef adc;
+    ADC_DeInit(ADC1);
+    adc.ADC_Mode = ADC_Mode_Independent;
+    adc.ADC_ScanConvMode = DISABLE;
+    adc.ADC_ContinuousConvMode = ENABLE;
+    adc.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+    adc.ADC_DataAlign = ADC_DataAlign_Right;
+    adc.ADC_NbrOfChannel = 1;
+    ADC_Init(ADC1, &adc);
+
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_55Cycles5);
+    ADC_DMACmd(ADC1, ENABLE);
+    ADC_Cmd(ADC1, ENABLE);
+
+    ADC_ResetCalibration(ADC1);
+    while (ADC_GetResetCalibrationStatus(ADC1));
+    ADC_StartCalibration(ADC1);
+    while (ADC_GetCalibrationStatus(ADC1));
+}
+
+void DMA_Config(void) {
+    DMA_InitTypeDef dma;
+    dma.DMA_PeripheralBaseAddr = (uint32_t)&ADC1->DR;
+    dma.DMA_MemoryBaseAddr = (uint32_t)&adc_buffer;
+    dma.DMA_DIR = DMA_DIR_PeripheralSRC;
+    dma.DMA_BufferSize = ADC_BUFFER_SIZE;
+    dma.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    dma.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    dma.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+    dma.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+    dma.DMA_Mode = DMA_Mode_Circular;
+    dma.DMA_Priority = DMA_Priority_High;
+    dma.DMA_M2M = DMA_M2M_Disable;
+    DMA_Init(DMA1_Channel1, &dma);
+
+    DMA_ITConfig(DMA1_Channel1, DMA_IT_TC, ENABLE);
+    NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+
+    DMA_Cmd(DMA1_Channel1, ENABLE);
+}
+
+
+void DMA1_Channel1_IRQHandler(void) {
+    if (DMA_GetITStatus(DMA1_IT_TC1)) {
+        DMA_ClearITPendingBit(DMA1_IT_TC1);
+    }
+}
+
+
+void delay_ms(uint32_t ms) {
+    for (uint32_t i = 0; i < ms * 8000; i++) {
+        __NOP(); 
+    }
+}
+
+
+void send_data(uint16_t *data, uint16_t size) {
+    char msg[32];
+    for (uint16_t i = 0; i < size; i++) {
+        sprintf(msg, "ADC[%d] = %d\r\n", i, data[i]);
+        for (char *p = msg; *p; p++) {
+            while (!(USART1->SR & USART_SR_TXE));
+            USART1->DR = *p;
         }
     }
-}
-/*Delay tuong doi*/
-void Delay(uint32_t t){
-    unsigned int i,j;
-    
-    for(i=0;i<t;i++){
-        for(j=0;j< 0x2AFF; j++);
-    }
-
-}
-void GPIO_Config(){
-    GPIO_InitTypeDef GPIO_InitStructure;
-    /*enble clock for GPIOC*/
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-    /*Configuration GPIO pin*/
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
-}
-void Clock_Config(void){
-    /* RCC system reset */
-    RCC_DeInit();
-    /* HCLK = SYSCLK */
-    RCC_HCLKConfig(RCC_SYSCLK_Div1); 
-    /* PCLK2 = HCLK */
-    RCC_PCLK2Config(RCC_HCLK_Div2);
-    /* PCLK1 = HCLK/2 */
-    RCC_PCLK1Config(RCC_HCLK_Div2);
-    /*enable HSI source clock*/
-    RCC_HSICmd(ENABLE); 
-    /* Wait till PLL is ready */
-    while (RCC_GetFlagStatus(RCC_FLAG_HSIRDY) == RESET){}
-    /* Select PLL as system clock source */
-    RCC_SYSCLKConfig(RCC_SYSCLKSource_HSI);
-    /* Wait till PLL is used as system clock source */
-    while(RCC_GetSYSCLKSource() != 0x00) {}    
 }
